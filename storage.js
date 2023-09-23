2fd3c3dfba1aa5ea2d4ae2f2b9f70b2a40033738
hemdheerreddy <hemdheerreddy>
2023-09-23 17:57:33 +0530

    store single user details

--- storage.js
+++ storage.js
@@ -1,9 +1,12 @@
 var inputs=document.getElementById('my-form');
 inputs.addEventListener('submit', function(e){
     e.preventDefault();
-    var nameInput = document.getElementById('name').value;
-    var emailInput = document.getElementById('email').value;
-    localStorage.setItem('username', nameInput);
-    localStorage.setItem('userEmail', emailInput);
-    // form.reset();
+    var nameInput= document.getElementById('name').value;
+    var emailInput= document.getElementById('email').value;
+    var userdetails={
+     name: nameInput,
+     email: emailInput
+    }
+    localStorage.setItem('userdetails', JSON.stringify(userdetails)); 
+     form.reset();
 })
